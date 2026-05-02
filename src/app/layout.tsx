b36cd0d4041e711c82cc44e84e266d083329a15d
Dhruv-AFK <dhruvjha1239@gmail.com>
2026-05-02 21:06:29 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "globals.css";
+//import "globals.css";
 import { Button } from "../components/ui";
 
 const geistSans = localFont({
